50279b601d3fb89a40088034f2e7c0607e256d32
riccardo-ottavi <ottavi.ps@libero.it>
2026-01-13 15:32:10 +0100

    inserisci colori colonna in base a status

---
+++ src/assets/components/TaskRow.jsx
@@ -0,0 +1,10 @@
+export default function TaskRow() {
+    return (
+        <div className="card">
+            <span>Nome</span>
+            <span>Stato</span>
+            <span>Data di Creazione</span>
+        </div>
+    )
+
+}
--- src/assets/components/TasksList.jsx
+++ src/assets/components/TasksList.jsx
@@ -1,16 +1,34 @@
 import { useGlobal } from "../../contexts/GlobalContext";
+import TaskRow from "./TaskRow";
 
 export default function TaskList() {
 
     const { tasks, getTasks } = useGlobal();
 
+    const STATUS_STYLE = {
+  "To do": {
+    backgroundColor: "#fef3c7",
+    color: "#92400e",
+  },
+  "Doing": {
+    backgroundColor: "#dbeafe",
+    color: "#1e40af",
+  },
+  "Done": {
+    backgroundColor: "#d1fae5",
+    color: "#065f46",
+  },
+};
+
     return (
         <div className="container">
-            <h2>Sono la lista delle task</h2>
+            <TaskRow />
             {tasks.map(task => (
                 <div className="card">
                     <span>{task.title}</span>
-                    <span>{task.status}</span>
+                    <span
+                        style={STATUS_STYLE[task.status]}
+                    >{task.status}</span>
                     <span>{task.createdAt}</span>
                 </div>
             ))}
