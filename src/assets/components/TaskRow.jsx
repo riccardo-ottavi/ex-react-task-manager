fbd46e9775c57107fffc8e64a1e2545bc24c0d16
riccardo-ottavi <ottavi.ps@libero.it>
2026-01-14 10:59:18 +0100

    refactoring della struttura delle righe

--- src/assets/components/TaskRow.jsx
+++ src/assets/components/TaskRow.jsx
@@ -1,9 +1,27 @@
-export default function TaskRow() {
+export default function TaskRow({ task }) {
+
+    const STATUS_STYLE = {
+        "To do": {
+            backgroundColor: "#fef3c7",
+            color: "#92400e",
+        },
+        "Doing": {
+            backgroundColor: "#dbeafe",
+            color: "#1e40af",
+        },
+        "Done": {
+            backgroundColor: "#d1fae5",
+            color: "#065f46",
+        },
+    };
+
     return (
-        <div className="card">
-            <span>Nome</span>
-            <span>Stato</span>
-            <span>Data di Creazione</span>
+        <div className="row">
+            <span>{task.title}</span>
+            <span
+                style={STATUS_STYLE[task.status]}
+            >{task.status}</span>
+            <span>{task.createdAt}</span>
         </div>
     )
 
--- src/assets/components/TasksList.jsx
+++ src/assets/components/TasksList.jsx
@@ -5,32 +5,12 @@ export default function TaskList() {
 
     const { tasks, getTasks } = useGlobal();
 
-    const STATUS_STYLE = {
-  "To do": {
-    backgroundColor: "#fef3c7",
-    color: "#92400e",
-  },
-  "Doing": {
-    backgroundColor: "#dbeafe",
-    color: "#1e40af",
-  },
-  "Done": {
-    backgroundColor: "#d1fae5",
-    color: "#065f46",
-  },
-};
-
     return (
         <div className="container">
-            <TaskRow />
             {tasks.map(task => (
-                <div className="card">
-                    <span>{task.title}</span>
-                    <span
-                        style={STATUS_STYLE[task.status]}
-                    >{task.status}</span>
-                    <span>{task.createdAt}</span>
-                </div>
+                <TaskRow 
+                    task={task}
+                />
             ))}
         </div>
     )
