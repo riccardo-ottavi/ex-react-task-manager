42eabe5299258c388ac8445d182b0ae0f109e59e
riccardo-ottavi <ottavi.ps@libero.it>
2026-01-14 11:10:10 +0100

    usa React.memo in componente row

--- src/assets/components/TaskRow.jsx
+++ src/assets/components/TaskRow.jsx
@@ -1,28 +1,30 @@
-export default function TaskRow({ task }) {
+import React from "react";
 
-    const STATUS_STYLE = {
-        "To do": {
-            backgroundColor: "#fef3c7",
-            color: "#92400e",
-        },
-        "Doing": {
-            backgroundColor: "#dbeafe",
-            color: "#1e40af",
-        },
-        "Done": {
-            backgroundColor: "#d1fae5",
-            color: "#065f46",
-        },
-    };
+const TaskRow = React.memo(({ task }) => {
+  const STATUS_STYLE = {
+    "To do": {
+      backgroundColor: "#fef3c7",
+      color: "#92400e",
+    },
+    Doing: {
+      backgroundColor: "#dbeafe",
+      color: "#1e40af",
+    },
+    Done: {
+      backgroundColor: "#d1fae5",
+      color: "#065f46",
+    },
+  };
 
-    return (
-        <div className="row">
-            <span>{task.title}</span>
-            <span
-                style={STATUS_STYLE[task.status]}
-            >{task.status}</span>
-            <span>{task.createdAt}</span>
-        </div>
-    )
+  return (
+    <div className="row">
+      <span>{task.title}</span>
+      <span style={STATUS_STYLE[task.status]}>
+        {task.status}
+      </span>
+      <span>{task.createdAt}</span>
+    </div>
+  );
+});
 
-}
+export default TaskRow;
