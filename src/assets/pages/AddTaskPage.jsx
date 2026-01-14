44cd56701af656f7938a86f6aa0e2a525853b1f8
riccardo-ottavi <ottavi.ps@libero.it>
2026-01-14 12:12:40 +0100

    inizializza form

--- src/assets/pages/AddTaskPage.jsx
+++ src/assets/pages/AddTaskPage.jsx
@@ -1,5 +1,27 @@
-export default function AddTask(){
-    return(
-        <h1>Sono la pagina Add Task</h1>
+import { useState, useRef } from "react";
+
+export default function AddTask() {
+
+    const [title, setTitle] = useState("");
+
+    function handleSubmit(e){
+        e.preventDefault();
+        console.log("submit", title);
+    }
+
+    return (
+        <div className="container">
+            <h1>Sono la pagina Add Task</h1>
+            <form action="" onSubmit={handleSubmit}>
+                <input type="text" value={title} onChange={(e) => setTitle(e.target.value)} />
+                <textarea name="" id=""></textarea>
+                <select name="" id="">
+                    <option value="">To Do</option>
+                    <option value="">Doing</option>
+                    <option value="">Done</option>
+                </select>
+                <button type="submit">Aggiungi Task</button>
+            </form>
+        </div>
     )
 }
