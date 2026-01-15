cd3bd93ccb80ce41847ff5d833a8ad46ff42897f
riccardo-ottavi <ottavi.ps@libero.it>
2026-01-15 15:02:12 +0100

    completa eliminazione con reindirizzamento alla lista

--- src/assets/pages/TaskDetaiPage.jsx
+++ src/assets/pages/TaskDetaiPage.jsx
@@ -1,11 +1,26 @@
 import { useParams } from "react-router-dom";
 import { useGlobal } from "../../contexts/GlobalContext";
+import { useNavigate } from "react-router-dom";
 
 export default function TaskDetailPage() {
   const { id } = useParams();
   const { tasks, removeTask } = useGlobal();
+  
+  // hook navigazione
+  const navigate = useNavigate(); 
 
   const task = tasks?.find(t => t.id === Number(id));
+
+  function handleRemove(){
+    try{
+        alert("Task rimossa con successo!")
+        removeTask(task?.id)       
+        navigate("/tasks")
+    }catch(err){
+        alert(err)
+    }
+    
+  }
 
   return (
     <div>
@@ -13,7 +28,7 @@ export default function TaskDetailPage() {
       <p>{task?.description}</p>
       <p>{task?.status}</p>
       <p>{task?.createdAt}</p>
-      <button onClick={() => removeTask(task?.id)}>Elimina Task</button>
+      <button onClick={handleRemove}>Elimina Task</button>
     </div>
   );
 }
