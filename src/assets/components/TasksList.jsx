5d74b2a7a339cacb764c2c79e4aaed5f8f4ed559
riccardo-ottavi <ottavi.ps@libero.it>
2026-01-16 10:26:08 +0100

    aggiungi intestazione tabella

--- src/assets/components/TasksList.jsx
+++ src/assets/components/TasksList.jsx
@@ -1,12 +1,21 @@
 import { useGlobal } from "../../contexts/GlobalContext";
 import TaskRow from "./TaskRow";
 import { Link } from "react-router-dom";
+import { useState } from "react";
 
 export default function TaskList() {
   const { tasks } = useGlobal();
+  const [sortBy, setSortBy] = useState(null);
+  const [sortOrder, setSortOrder] = useState(1);
+
 
   return (
     <div className="container">
+        <div className="row">
+      <span>Titolo</span>
+      <span>Status</span>
+      <span>Data Creazione</span>
+    </div>
       {tasks?.map(task => (
         <Link key={task.id} to={`/tasks/${task.id}`} >
             <TaskRow
