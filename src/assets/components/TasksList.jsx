f5598f62a267bdda002a0af5c4c4e1e842f00190
riccardo-ottavi <ottavi.ps@libero.it>
2026-01-16 11:57:23 +0100

    implementa ordinamento tasks

--- src/assets/components/TasksList.jsx
+++ src/assets/components/TasksList.jsx
@@ -1,22 +1,40 @@
 import { useGlobal } from "../../contexts/GlobalContext";
 import TaskRow from "./TaskRow";
 import { Link } from "react-router-dom";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
 export default function TaskList() {
   const { tasks } = useGlobal();
-  const [sortBy, setSortBy] = useState(null);
+
+  const [sortBy, setSortBy] = useState("createdAt");
   const [sortOrder, setSortOrder] = useState(1);
 
+
+  function handleSort(column){
+    if (sortBy === column) {
+    setSortOrder(sortOrder * -1);
+  } else {
+    setSortBy(column);
+    setSortOrder(1);
+  }
+  }
+
+  //algoritmo ordinamento tasks
+  const sortedTasks = [...tasks].sort((a, b) => {
+  if (!sortBy) return 0; 
+  if (a[sortBy] < b[sortBy]) return -1 * sortOrder;
+  if (a[sortBy] > b[sortBy]) return 1 * sortOrder;
+  return 0;
+});
 
   return (
     <div className="container">
         <div className="row">
-      <span>Titolo</span>
-      <span>Status</span>
-      <span>Data Creazione</span>
+      <span onClick={() => handleSort("title")}>Titolo</span>
+      <span onClick={() => handleSort("status")}>Status</span>
+      <span onClick={() => handleSort("createdAt")}>Data Creazione</span>
     </div>
-      {tasks?.map(task => (
+      {sortedTasks?.map(task => (
         <Link key={task.id} to={`/tasks/${task.id}`} >
             <TaskRow
             key={task.id}
