a9399cdfbaa2d5495c613e29adc3ec2a2a669075
riccardo-ottavi <ottavi.ps@libero.it>
2026-01-16 08:32:25 +0100

    randerizza modal usando portal

--- src/assets/components/Modal.jsx
+++ src/assets/components/Modal.jsx
@@ -1,12 +1,16 @@
+import ReactDOM from "react-dom";
+
 export default function Modal({ title, content, show, onClose, onConfirm, confirmText }){
     
     
-    return show && (
+    return show && ReactDOM.createPortal(
         <div className="container-modal">
             <h2>{title}</h2>
             <p>{content}</p>
-            <button onClick={onClose}>Close</button>
-        </div>
+            <button onClick={onClose}>Chiudi</button>
+            <button onClick={onConfirm}>Conferma</button>
+        </div>,
+        document.body
     )
 }
 
