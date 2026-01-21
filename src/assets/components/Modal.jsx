5f7d490f624a83cbafe695c79e53bf19a808ffd7
riccardo-ottavi <ottavi.ps@libero.it>
2026-01-21 13:32:07 +0100

    utilizza confirmText nella modale di rimozione

--- src/assets/components/Modal.jsx
+++ src/assets/components/Modal.jsx
@@ -1,14 +1,13 @@
 import ReactDOM from "react-dom";
 
-export default function Modal({ title, content, show, onClose, onConfirm, confirmText }){
+export default function Modal({ title, content, show, onClose, onConfirm, confirmText = "Conferma" }){
     
     return show && ReactDOM.createPortal(
         <div className="container-modal">
-            {/*Promemoria: inserisci stile modale e usa confirmText */}
             <h2>{title}</h2>
             <span>{content}</span>
             <button onClick={onClose}>Chiudi</button>
-            <button onClick={onConfirm}>Conferma</button>
+            <button onClick={onConfirm}>{confirmText}</button>
         </div>,
         document.body
     )
